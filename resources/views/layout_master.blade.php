1591867137715da551b1f0a72be240a135aa40d2
SaraBarrett <sara.c.mon@gmail.com>
2026-01-17 15:27:51 +0000

    style changes

--- resources/views/layout_master.blade.php
+++ resources/views/layout_master.blade.php
@@ -4,13 +4,23 @@
 <head>
     <meta charset="utf-8">
     <title>MedPharma</title>
+
+    <link rel="icon" type="image/x-icon" href="{{asset('images/identity/MedPharma_logof.png')}}">
+
+    <script src="{{ asset('js/script.js') }}"></script>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="">MedPharma</a>
+            <a class="navbar-brand" href="{{route('client.home')}}">
+                <img src="{{ asset('images/identity/MedPharma_logo.png') }}" alt="MedPharma Logo"
+                    class="d-inline-block logo">
+                MedPharma
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -20,7 +30,7 @@
                     <li class="nav-item"><a class="nav-link" href="{{ route('client.gallery') }}">Gallery</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('client.booking_form') }}">Booking</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('client.about') }}">About Us</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{route('client.contact')}}">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('client.contact') }}">Contact</a></li>
                     {{-- Link Admin só para autenticados (opcional) --}}
                     @auth
                         <li class="nav-item">
